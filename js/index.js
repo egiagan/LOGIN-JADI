bd47b9b5bacb036ca58250ba46b918a3caa7912f
egiagan <30861646+egiagan@users.noreply.github.com>
2025-01-07 22:08:07 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,9 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyDAlth1sxiP-S3r3fVDXhwadVVnEvpdO6s",
-  authDomain: "login-egi-agan.firebaseapp.com",
-  databaseURL: "https://login-egi-agan-default-rtdb.firebaseio.com",
-  projectId: "login-egi-agan",
-  storageBucket: "login-egi-agan.firebasestorage.app",
-  messagingSenderId: "395059466114",
-  appId: "1:395059466114:web:5c6b0621e9739df6b5c99b"
-};
+ " enter here your firebase credentials
+  };
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
-
 
 
   firebase.auth.Auth.Persistence.LOCAL;
